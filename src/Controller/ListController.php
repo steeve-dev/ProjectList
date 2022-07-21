9a51f779e4d00e7b3b29a85ff0e00c2949f4e766
steeve mellet <melletsteeve@orange.fr>
2022-07-21 23:29:22 +0200

    create list page and logical

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -15,14 +15,22 @@ class ListController extends AbstractController
     #[Route('/list', name: 'ListPage')]
     public function ListPage(Request $request, EntityManagerInterface $entityManager): Response
     {
+        //create a new list
+
         $list = new UserList();
         $form = $this->createForm(NewListType::class, $list);
 
         $form->handleRequest($request);
+
+        // get the list of the current user
+
         $user = $this->getUser();
-        $userList = $entityManager->getRepository(UserList::class)->findBy(['user'=>$user]);
-        
-        if ($form->isSubmitted() && $form->isValid()){
+        $userList = $entityManager->getRepository(UserList::class)->findBy(['user' => $user]);
+
+
+        //Submit the form list
+
+        if ($form->isSubmitted() && $form->isValid()) {
 
             $list->setUser($this->getUser());
             $entityManager->persist($list);
@@ -31,6 +39,8 @@ class ListController extends AbstractController
             return $this->redirectToRoute('ListPage');
 
         }
+
+
 
         return $this->render('listPage.html.twig', [
             'lists'=>$userList,
--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -5,4 +5,6 @@ namespace App\Controller;
 class TaskController
 {
 
+
+
 }
