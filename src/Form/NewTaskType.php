a28b5a2f3317c744639d974165ccabbf7b9dd4b1
steeve mellet <melletsteeve@orange.fr>
2022-07-23 12:40:23 +0200

    ajout de la page compte

--- src/Form/NewTaskType.php
+++ src/Form/NewTaskType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
